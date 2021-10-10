a9b1191c2d4dc347eeec6cddbfe0defbd901df3f
Shadic78 <carlosjosue7889@gmail.com>
2021-10-10 15:59:45 -0500

    Added delete and update services for User entity

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,6 @@
 import { User } from 'entities/user.entity';
 import { UserRepository } from 'repositories/user.repository';
+import * as bcrypt from 'bcrypt';
 
 export const UserService = (userRepository: UserRepository) => {
   const getAllUsers = async () => {
@@ -17,12 +18,41 @@ export const UserService = (userRepository: UserRepository) => {
   };
 
   const createUser = async (user: User) => {
-    // Hashear el password.
-
+    const userIsRegistered = getUserByEmail(user.email);
+    if(userIsRegistered) {
+      return Promise.reject('El email ya esta registrado');
+    }
+    user.password = await hashPassword(user.password);
     const newUser = await userRepository.save(user);
-
     return newUser;
   };
 
-  return { getAllUsers, getUserById, createUser };
+  const deleteUserById = async (id: string) => {
+    const parsedId = parseInt(id);
+    if(isNaN(parsedId)) {
+      return Promise.reject('ID inválido');
+    }
+    return await userRepository.delete({ id: parsedId });
+  };
+
+  const updateUser = async (id: string, newUserData: User) => {
+    const result = await getUserById(id);
+    const newPassword = await hashPassword(newUserData.password);
+    const updatedUser = {...result, ...newUserData, password: newPassword};
+    return await userRepository.save(updatedUser);
+  };  
+
+  const hashPassword = async (password: string) => {
+    return await bcrypt.hash(password, 10);
+  }  
+
+  const getUserByEmail = async (email: string) => {
+    const user = await userRepository.findOne({ email });
+    if (!user) {
+      return Promise.reject(`No se encontro un usuario con el email: ${email}`);
+    }
+    return user;
+  }  
+
+  return { getAllUsers, getUserById, createUser, deleteUserById, updateUser };
 };
