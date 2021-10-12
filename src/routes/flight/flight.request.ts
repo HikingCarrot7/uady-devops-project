600baa237a191828a1155d398326e7ec9e881e74
HikingCarrot7 <ricardoibarra2044@gmail.com>
2021-10-11 19:48:02 -0500

    fail test

--- src/routes/flight/flight.request.ts
+++ src/routes/flight/flight.request.ts
@@ -1,4 +1,4 @@
-import { Matches, IsNumber, Min, Max } from 'class-validator';
+import { IsNumber, Matches, Max, Min } from 'class-validator';
 
 export class FlightRequest {
   @Matches(/^\d{4}(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$/i, {
